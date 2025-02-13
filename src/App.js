f21f5b150fa96b981ac20bc4f914b0c53d265576
Lasitha Udayanga <ludayanga@ariasystems.com>
2025-02-13 15:01:28 +0530

    Added feature to carousel when higher number of nodes are available

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, NavLink } from "react-router-dom";
 import Home from "./home/home";
 import DefaultChart from "./default-chart/default-chart";
 import PanZoomChart from "./pan-zoom-chart/pan-zoom-chart";
@@ -13,18 +13,17 @@ import "./App.css";
 const App = () => {
   return (
     <Router>
-      <div className="wrapper">
-        <nav>
-          <NavLink to="/" exact activeClassName="selected">Home</NavLink>
-          <NavLink to="/default-chart" activeClassName="selected">Deault Chart</NavLink>
-          <NavLink to="/pan-zoom-chart" activeClassName="selected">Pan & Zoom Chart</NavLink>
-          <NavLink to="/custom-node-chart" activeClassName="selected">Custom Node Chart</NavLink>
-          <NavLink to="/export-chart" activeClassName="selected">Export Chart</NavLink>
-          <NavLink to="/drag-drop-chart" activeClassName="selected">Drap & Drop Chart</NavLink>
-          <NavLink to="/edit-chart" activeClassName="selected">Edit Chart</NavLink>
-          <NavLink to="/edit-node" activeClassName="selected">Edit Node</NavLink>
-        </nav>
-
+      <nav>
+        <NavLink exact to="/" activeClassName="selected">Home</NavLink>
+        <NavLink to="/default-chart" activeClassName="selected">Default Chart</NavLink>
+        <NavLink to="/pan-zoom-chart" activeClassName="selected">Pan Zoom Chart</NavLink>
+        <NavLink to="/custom-node-chart" activeClassName="selected">Custom Node Chart</NavLink>
+        <NavLink to="/export-chart" activeClassName="selected">Export Chart</NavLink>
+        <NavLink to="/drag-drop-chart" activeClassName="selected">Drag Drop Chart</NavLink>
+        <NavLink to="/edit-chart" activeClassName="selected">Edit Chart</NavLink>
+        <NavLink to="/edit-node" activeClassName="selected">Edit Node</NavLink>
+      </nav>
+      <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/default-chart" component={DefaultChart} />
         <Route path="/pan-zoom-chart" component={PanZoomChart} />
@@ -33,7 +32,8 @@ const App = () => {
         <Route path="/drag-drop-chart" component={DragDropChart} />
         <Route path="/edit-chart" component={EditChart} />
         <Route path="/edit-node" component={EditNode} />
-      </div>
+        <Route path="*" component={Home} />
+      </Switch>
     </Router>
   );
 };
